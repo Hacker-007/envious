4dd81cab2d669c6b489d4b9b1baea999a2243641
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2021-04-12 14:23:36 -0700

    Tested solution

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 extern "C" {
     char hello(char);
+    int test();
 }
 
 int main() {
     char c = 'd';
-    std::cout << "test('" << c << "') = " << hello(c) << std::endl;
+    std::cout << "hello('" << c << "') = " << hello(c) << std::endl;
+    std::cout << "test() = " << test() << std::endl;
 }
 
